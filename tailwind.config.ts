37ce030d0d71cb701da93c212070738f86c1ea2f
Martefay <dilenmartefayy@gmail.com>
2024-03-14 19:59:12 +0300

    navbar gotov

---
+++ tailwind.config.ts
@@ -0,0 +1,35 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding: '15px'
+    },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1440px',
+    },
+    fontFamily: {
+      oswald: 'var(--font-oswald)',
+      roboto: 'var(--font-roboto)',
+    },
+    backgroundImage: {
+      hero: 'url(/assets/img/hero/img_1.jpg)',
+    },
+    extend: {
+      colors: {
+        accent: '#0000ff',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
